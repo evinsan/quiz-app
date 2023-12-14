f7484f7416d825ce23f13dc4b0b67110dd0e2ac6
evinsan <153627861+evinsan@users.noreply.github.com>
2023-12-14 16:08:19 +0200

    server.js

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const fs = require('fs');
-const path = require('path');
-const cors = require('cors');
-
 const app = express();
 const port = process.env.PORT || 3000;
-const rankingsFilePath = path.join(__dirname, 'rankings.json');
+
 
 app.use(express.json());
 app.use(express.static('public'));
-app.use(cors()); // Enable CORS for all routes
 
 app.get('/', (req, res) => {
-    const indexPath = path.join(__dirname, 'public', 'index.html');
-    res.sendFile(indexPath);
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 app.get('/api/rankings', (req, res) => {
@@ -31,10 +26,9 @@ app.post('/api/rankings', (req, res) => {
 
 function readRankings() {
     try {
-        const data = fs.readFileSync(rankingsFilePath, 'utf8');
+        const data = fs.readFileSync(__dirname + '/rankings.json', 'utf8');
         return JSON.parse(data);
     } catch (error) {
-        console.error('Error reading rankings:', error.message);
         return [];
     }
 }
@@ -42,12 +36,7 @@ function readRankings() {
 function addResult(result) {
     const rankings = readRankings();
     rankings.push(result);
-
-    try {
-        fs.writeFileSync(rankingsFilePath, JSON.stringify(rankings, null, 2), 'utf8');
-    } catch (error) {
-        console.error('Error writing rankings:', error.message);
-    }
+    fs.writeFileSync(__dirname + '/rankings.json', JSON.stringify(rankings, null, 2), 'utf8');
 }
 
 app.listen(port, () => {
